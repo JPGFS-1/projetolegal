744b35627e9bd2b2693e2d826eae93dc8e100444
jpgfs <jose>
2025-08-26 17:40:08 -0300

    Atualização

---
+++ Projeto/src/approutes.jsx
@@ -0,0 +1,13 @@
+import { BrawserRouter, Routes, Route, Link } from "react-router-dom";
+
+export default function Approutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={} />
+        <Route path="/" element={} />
+        <Route path="/" element={} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- Projeto/src/routes.jsx
+++
@@ -1,7 +0,0 @@
-export default function Routes() {
-  return (
-    <header>
-      <h1>My Application</h1>
-    </header>
-  );
-}
