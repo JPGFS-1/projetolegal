159d3e1047d69d2c0197b0f3debd95d2fcc01c40
jpgfs <jose>
2025-08-26 17:27:22 -0300

    Atualização

---
+++ Projeto/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Meu Projeto Legal</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Sobre</a>
+        <a href="#">Contato</a>
+      </nav>
+    </header>
+  );
+}
